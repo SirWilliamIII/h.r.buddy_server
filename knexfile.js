37180a7be97103f491e54ef36427db5d8fe45541
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-27 07:21:38 -0600

    added db

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
 	},
   production: {
     client:     'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+    	database: 'postgres://dehuwseiapwenw:7329ef911bbe439533eb947b086b7971547b7b73cdb28a5639be2f19ab3fb0d1@ec2-23-23-159-84.compute-1.amazonaws.com:5432/ddvtr90p6df9cn',
+	    user: 'dev.wtc3@gmail.com',
+	    password: '4unIver$e2'
+    }
   }
 }
