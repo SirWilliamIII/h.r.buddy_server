abba98e135d102b0b477e93ef01ec54b3501d230
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-27 17:40:20 -0600

    switched queries to raw SQL

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,17 @@ const router = express.Router()
 const knex = require('../db/knex')
 
 //  Get all employees
+// router.get('/', (req, res) => {
+// 	knex('employees')
+// 		.then(employee => {
+// 			res.json({
+// 				employee
+// 			})
+// 		})
+// })
+
 router.get('/', (req, res) => {
-	knex('employees')
+	knex.raw('select * from employees')
 		.then(employee => {
 			res.json({
 				employee
@@ -17,22 +26,21 @@ router.get('/', (req, res) => {
 // Add an employee
 
 
-
 router.post('/', (req, res) => {
 	let newEmployee = {
-		first_name:  req.body.first_name,
-		last_name:   req.body.last_name,
-		email:      req.body.email,
-		phone:      req.body.phone,
-		address:    req.body.address,
-		city:       req.body.city,
-		state:      req.body.state,
-		department: req.body.department
+		first:   req.body.first_name,
+		last:    req.body.last_name,
+		email:   req.body.email,
+		phone:   req.body.phone,
+		address: req.body.address,
+		city:    req.body.city,
+		state:   req.body.state,
+		dept:    req.body.department
 	}
-    return knex('employees').insert(newEmployee)
-    .then(employee => {
-      res.json(employee)
-    });
+	return knex('employees').insert(newEmployee)
+		.then(employee => {
+			res.json(employee)
+		});
 });
 
 module.exports = router
