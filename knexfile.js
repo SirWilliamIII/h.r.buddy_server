8d9e502c1b26118d6e4f05b6cbe5c05513180f48
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-27 07:45:38 -0600

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,9 @@ module.exports = {
 		connection: 'postgres://localhost/hr_bud'
 	},
   production: {
-    client:     'pg',
+    client:     'postgresql',
     connection: {
+    	 port: 5432,
     	database: 'postgres://dehuwseiapwenw:7329ef911bbe439533eb947b086b7971547b7b73cdb28a5639be2f19ab3fb0d1@ec2-23-23-159-84.compute-1.amazonaws.com:5432/ddvtr90p6df9cngs',
 	    user: 'dev.wtc3@gmail.com',
 	    password: '4unIver$e2'
