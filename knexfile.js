b9feb6f8a137ddfb4415214235e9529a76b85825
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-27 09:39:29 -0600

    changed knex

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,7 @@ module.exports = {
 		connection: 'postgres://localhost/hr_bud'
 	},
   production: {
-    client:     'postgresql',
-    connection: {
-    	database: 'postgres://dehuwseiapwenw:7329ef911bbe439533eb947b086b7971547b7b73cdb28a5639be2f19ab3fb0d1@ec2-23-23-159-84.compute-1.amazonaws.com:5432/ddvtr90p6df9cngs',
-	    user: 'sirwilliamiii',
-	    password: '4unIver$e2'
-    }
+    client:     'pg',
+    connection: process.env.DATABASE_URL
   }
 }
